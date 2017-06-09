b05df0de0dcb3937d68862917a3018829070961d
Nehhaket <nehhaket@gmail.com>
2017-06-09 11:37:06 +0200

    Further clean up

--- server.js
+++ server.js
@@ -1,89 +1,89 @@
-const app = require('express')()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
-const activeClientsTable = {}
-const areTyping = {}
+const activeClientsTable = {};
+const areTyping = {};
 
 
 //requests handlers
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+  res.sendFile(__dirname + '/index.html');
+});
 app.get('/user.js', (req, res) => {
-  res.sendFile(__dirname + '/user.js')
-})
+  res.sendFile(__dirname + '/user.js');
+});
 app.get('/socket.io.js', (req, res) => {
-  res.sendFile(__dirname + '/socket.io.js')
-})
+  res.sendFile(__dirname + '/socket.io.js');
+});
 
 
 //chat event handlers
 io.on('connection', (socket) => {
   //user creation handler
   socket.on('user creation', (username) => {
-    let message
-    const tmp = activeClientsTable[socket.id]
+    let message;
+    const tmp = activeClientsTable[socket.id];
     if (tmp == undefined) {
       if (username == "") {
-        return
+        return;
       }
       else {
-        message = username + " connected"
+        message = username + " connected";
       }
     }
     else if (tmp == username) {
-      return
+      return;
     }
     else {
-      message = tmp + " changed username to " + username
+      message = tmp + " changed username to " + username;
     }
-    activeClientsTable[socket.id] = username
-    console.log(message + "\t\t(" + socket.id + ")")
-    socket.broadcast.emit('server message', message)
-  })
+    activeClientsTable[socket.id] = username;
+    console.log(message + "\t\t(" + socket.id + ")");
+    socket.broadcast.emit('server message', message);
+  });
 
   //messages handler
   socket.on('client message', (msg) => {
-    const user = activeClientsTable[socket.id]
+    const user = activeClientsTable[socket.id];
     if (user != undefined) {
-      const message = user + ": " + msg
-      console.log(message + "\t\t(" + socket.id + ")")
-      socket.broadcast.emit('server message', message)
+      const message = user + ": " + msg;
+      console.log(message + "\t\t(" + socket.id + ")");
+      socket.broadcast.emit('server message', message);
     }
     else {
-      socket.emit('server message', "You need a username to use the chat!")
+      socket.emit('server message', "You need a username to use the chat!");
     }
-  })
+  });
 
   //'user is typing'
   socket.on('typing', (bool) => {
     if (bool) {
-      areTyping[socket.id] = activeClientsTable[socket.id]
+      areTyping[socket.id] = activeClientsTable[socket.id];
     }
     else if (areTyping[socket.id] != undefined){
-      delete areTyping[socket.id]
+      delete areTyping[socket.id];
     }
-    socket.broadcast.emit('typing', areTyping)
+    socket.broadcast.emit('typing', areTyping);
   })
 
   //client disconnection handler
   socket.on('disconnect', () => {
-    const user = activeClientsTable[socket.id]
+    const user = activeClientsTable[socket.id];
     if (user != undefined) {
-      io.emit('server message', user + " disconnected")
-      console.log(user + ' disconnected')
-      delete activeClientsTable[socket.id]
+      io.emit('server message', user + " disconnected");
+      console.log(user + ' disconnected');
+      delete activeClientsTable[socket.id];
       if (areTyping[socket.id] != undefined) {
-        delete areTyping[socket.id]
-        socket.broadcast.emit('typing', areTyping)
+        delete areTyping[socket.id];
+        socket.broadcast.emit('typing', areTyping);
       }
     }
-  })
-})
+  });
+});
 
 
 //http listener
 http.listen(PORT, () => {
-  console.log('listening on port ' + PORT)
-})
+  console.log('listening on port ' + PORT);
+});
--- user.js
+++ user.js
@@ -1,82 +1,81 @@
 $(function() {
-  let username = ""
-  const socket = io()
+  let username = "";
+  const socket = io();
   const areTyping = {
     data : "",
     index : 0,
     updateData: function(who) {
       const keysTable = Object.keys(who)
-                        .filter( (e) => who[e]!=username )
+                        .filter( (e) => who[e]!=username );
       if (keysTable.length == 0) {
-        this.data = ""
+        this.data = "";
       }
       else if (keysTable.length == 1) {
-        this.data = who[keysTable[0]] + " is typing..."
+        this.data = who[keysTable[0]] + " is typing...";
       }
       else {
-        console.log(who)
-        this.data = " are typing..."
-        let tmp = ""
+        console.log(who);
+        this.data = " are typing...";
+        let tmp = "";
         for(let i=0; i < keysTable.length; i++) {
-          tmp = who[keysTable[i]] + ", " + tmp
+          tmp = who[keysTable[i]] + ", " + tmp;
         }
-        this.data = tmp.slice(0,-2) + this.data
+        this.data = tmp.slice(0,-2) + this.data;
       }
     }
   }
-  $('#messages').append($('<li>').text(areTyping.data))
+  $('#messages').append($('<li>').text(areTyping.data));
 
   //username submiter
   $('#usr').submit( () => {
-    username = $('#username').val()
-    socket.emit('user creation', username)
-    $('#messages')[0].childNodes[areTyping.index].remove()
-    $('#messages').append($('<li>').text("Connected!"))
-    $('#messages').append($('<li>').text(areTyping.data))
-    areTyping.index += 1
-    return false
+    username = $('#username').val();
+    socket.emit('user creation', username);
+    $('#messages')[0].childNodes[areTyping.index].remove();
+    $('#messages').append($('<li>').text("Connected!"));
+    $('#messages').append($('<li>').text(areTyping.data));
+    areTyping.index += 1;
+    return false;
   })
 
   //message sender
   $('#msg').submit( () => {
-    const msg = $('#m').val()
-    if (msg == "") return false
-    $('#m').val('')
-    //$('#m').blur()
-    socket.emit('typing', false)
-    socket.emit('client message', msg)
-    $('#messages')[0].childNodes[areTyping.index].remove()
-    $('#messages').append($('<li>').text("Me: " + msg))
-    $('#messages').append($('<li>').text(areTyping.data))
-    areTyping.index += 1
-    return false
-  })
+    const msg = $('#m').val();
+    if (msg == "") return false;
+    $('#m').val('');
+    socket.emit('typing', false);
+    socket.emit('client message', msg);
+    $('#messages')[0].childNodes[areTyping.index].remove();
+    $('#messages').append($('<li>').text("Me: " + msg));
+    $('#messages').append($('<li>').text(areTyping.data));
+    areTyping.index += 1;
+    return false;
+  });
 
 
   //message listener
   socket.on('server message', (msg) => {
-    $('#messages')[0].childNodes[areTyping.index].remove()
-    $('#messages').append($('<li>').text(msg))
-    $('#messages').append($('<li>').text(areTyping.data))
-    areTyping.index += 1
-  })
+    $('#messages')[0].childNodes[areTyping.index].remove();
+    $('#messages').append($('<li>').text(msg));
+    $('#messages').append($('<li>').text(areTyping.data));
+    areTyping.index += 1;
+  });
 
 
   //am I typing?
   $('#m').bind('input', () => {
     if ($('#m').val() != "") {
-      socket.emit('typing', true)
+      socket.emit('typing', true);
     }
     else {
-      socket.emit('typing', false)
+      socket.emit('typing', false);
     }
   })
 
 
   //'{user} is typing'
   socket.on('typing', (typers) => {
-    areTyping.updateData(typers)
-    $('#messages')[0].childNodes[areTyping.index].remove()
-    $('#messages').append($('<li>').text(areTyping.data))
-  })
-})
+    areTyping.updateData(typers);
+    $('#messages')[0].childNodes[areTyping.index].remove();
+    $('#messages').append($('<li>').text(areTyping.data));
+  });
+});
